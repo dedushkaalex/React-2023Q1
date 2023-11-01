699dd6c145f15328ad9b13774ba4d24be0e31be0
Aleksei Erchenko <proger@a1exxuss.ru>
2023-11-01 21:42:52 +0300

    feature: Input component Class to FC

--- src/views/Elements/Input/Input.tsx
+++ src/views/Elements/Input/Input.tsx
@@ -1,35 +1,26 @@
-import { Component, HTMLAttributes } from 'react';
+import { FC, HTMLAttributes } from 'react';
 import styles from './Input.module.css';
 
-interface Props extends Omit<HTMLAttributes<HTMLInputElement>, 'placeholder'> {
-  placeholder: string;
+interface InputProps extends HTMLAttributes<HTMLInputElement> {
+  placeholder?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   value: string;
 }
 
-interface State {}
-
-class Input extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-  }
-  render() {
-    return (
-      <label className={styles.field}>
-        <input
-          type="text"
-          id="search"
-          className={styles.input__field}
-          autoComplete="off"
-          {...this.props}
-          placeholder=""
-          onChange={this.props.onChange}
-          value={this.props.value}
-        />
-        <span className={styles.label}>{this.props.placeholder || ''}</span>
-      </label>
-    );
-  }
-}
-
-export default Input;
+export const Input: FC<InputProps> = ({ placeholder = '', value, onChange, ...props }) => {
+  return (
+    <label className={styles.field}>
+      <input
+        type="text"
+        id="search"
+        className={styles.input__field}
+        autoComplete="off"
+        {...props}
+        placeholder=""
+        onChange={onChange}
+        value={value}
+      />
+      <span className={styles.label}>{placeholder || ''}</span>
+    </label>
+  );
+};
